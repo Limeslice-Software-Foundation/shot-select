0f38ff6c7173f7422440b1382d2343b521a666ce
Trevor <tcmiller.dev@proton.me>
2024-09-18 08:42:49 +1000

    Add basic application architecture

---
+++ lib/injection.config.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+import 'repository/raw_file_repository.dart' as _i270;
+import 'service/raw_file_service.dart' as _i772;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    gh.lazySingleton<_i270.RawFileRepository>(() => _i270.RawFileRepository());
+    gh.lazySingleton<_i772.RawFileService>(() => _i772.RawFileService());
+    return this;
+  }
+}
---
+++ lib/state/notifier/raw_file_state_notifier.dart
@@ -0,0 +1,44 @@
+// Licensed to the Limeslice Software Foundation (LSF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The LSF licenses this file to You under the MIT License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://limeslice.org/license.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../injection.dart';
+import '../../model/raw_file.dart';
+import '../../service/raw_file_service.dart';
+import '../raw_file_state.dart';
+
+class RawFileStateNotifier extends StateNotifier<RawFileState> {
+  RawFileStateNotifier() : super(const RawFileState(files: []));
+
+  Future<void> loadFiles(String directory) async {
+    state = state.copyWith(files: [], isError: false, isLoading: true);
+
+    try {
+      RawFileService service = getIt<RawFileService>();
+      List<RawFile> files = await service.loadFiles(directory);
+      state = state.copyWith(files: files);
+    } catch (err) {
+      state = state.copyWith(isError: true);
+    }
+    finally {
+      state = state.copyWith(isLoading: false);
+    }
+  }
+
+  Future<void> closeAllFiles() async {
+    RawFileService service = getIt<RawFileService>();
+    await service.closeAll(state.files);
+  }
+}
